fa4777f948be2f7dc3d87af3e5419c743adf7fa9
Artur Frolov <djajikamail@gmail.com>
2026-03-10 17:42:33 +0200

    Update `package-lock.json` and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,63 +10,63 @@ const dist = "./dist";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
-                .pipe(gulp.dest(dist))
-                .pipe(browsersync.stream());
+        .pipe(gulp.dest(dist))
+        .pipe(browsersync.stream());
 });
 
 gulp.task("build-js", () => {
     return gulp.src("./src/js/main.js")
-                .pipe(webpack({
-                    mode: 'development',
-                    output: {
-                        filename: 'main.js'
-                    },
-                    watch: false,
-                    devtool: "source-map",
-                    module: {
-                        rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
+        .pipe(webpack({
+            mode: 'development',
+            output: {
+                filename: 'main.js'
+            },
+            watch: false,
+            devtool: "source-map",
+            module: {
+                rules: [
+                    {
+                        test: /\.m?js$/,
+                        exclude: /(node_modules|bower_components)/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
                                 presets: [['@babel/preset-env', {
                                     debug: true,
                                     corejs: 3,
                                     useBuiltIns: "usage"
                                 }]]
-                              }
                             }
-                          }
-                        ]
-                      }
-                }))
-                .pipe(gulp.dest(dist + '/js'))
-                .pipe(browsersync.stream());
+                        }
+                    }
+                ]
+            }
+        }))
+        .pipe(gulp.dest(dist + '/js'))
+        .pipe(browsersync.stream());
 });
 
 gulp.task("build-sass", () => {
     return gulp.src("./src/scss/**/*.scss")
-                .pipe(sass().on('error', sass.logError))
-                .pipe(gulp.dest(dist + '/css'))
-                .pipe(browsersync.stream());
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(dist + '/css'))
+        .pipe(browsersync.stream());
 });
 
 gulp.task("copy-icons", () => {
     return gulp.src("./src/icons/**/*.*")
-               .pipe(gulp.dest(dist + "/icons"));
+        .pipe(gulp.dest(dist + "/icons"));
 });
 
 gulp.task("copy-img", () => {
     return gulp.src("./src/img/**/*.*")
-               .pipe(gulp.dest(dist + "/img"))
-               .pipe(browsersync.stream());
+        .pipe(gulp.dest(dist + "/img"))
+        .pipe(browsersync.stream());
 });
 
 gulp.task("copy-assets", gulp.parallel("copy-icons", "copy-img"));
 
-gulp.task("watch", () => {
+gulp.task("watch", (done) => {
     browsersync.init({
         server: "./dist/",
         port: 4000,
@@ -78,6 +78,7 @@ gulp.task("watch", () => {
     gulp.watch("./src/img/**/*.*", gulp.parallel("copy-img"));
     gulp.watch("./src/scss/**/*.scss", gulp.parallel("build-sass"));
     gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
+    done();
 });
 
 gulp.task("build", gulp.parallel("copy-html", "copy-assets", "build-sass", "build-js"));
@@ -91,22 +92,22 @@ gulp.task("prod-js", () => {
             },
             module: {
                 rules: [
-                  {
-                    test: /\.m?js$/,
-                    exclude: /(node_modules|bower_components)/,
-                    use: {
-                      loader: 'babel-loader',
-                      options: {
-                        presets: [['@babel/preset-env', {
-                            debug: false,
-                            corejs: 3,
-                            useBuiltIns: "usage"
-                        }]]
-                      }
+                    {
+                        test: /\.m?js$/,
+                        exclude: /(node_modules|bower_components)/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [['@babel/preset-env', {
+                                    debug: false,
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                            }
+                        }
                     }
-                  }
                 ]
-              }
+            }
         }))
         .pipe(gulp.dest(dist + '/js'));
 });
